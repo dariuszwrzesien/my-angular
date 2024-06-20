73af29070ddcc5f4b7c328f004f833a4bfec7c8f
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2024-06-20 15:36:01 +0200

    chore: Update HTML structure and CSS classes for components

--- my-angular-course-18/01-starting-project/src/app/shared/control/control.component.ts
+++ my-angular-course-18/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,7 +14,22 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None, // Style CSS stają się globalne
+  host: {
+    class: 'control', //przypisuje classe CSSową do hosta teraz wszędzie jak zobaczymy w Inspectorze to będzie <app-control class="control">
+    '(click)': 'onClick()', // nasłuchiwanie na eventy
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; // stary sposób nadawania klasy CSS w host
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }// nasłuchiwanie na eventy
   label = input.required<string>();
+  private el = inject(ElementRef); // daje dostęp do tego elemetnu(komponentu) (host)
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el.nativeElement);
+  }
 }
