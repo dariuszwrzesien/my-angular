bd4aff9b2a31b24645498f6004977895193cfa35
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2024-06-22 18:12:06 +0200

    chore: Update HTML structure and CSS classes for components

--- my-angular-course-18/01-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ my-angular-course-18/01-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type ServerStatus = 'online' | 'offline' | 'unknown';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: ServerStatus = 'online';
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentStatus = this.getRandomStatus();
+    }, 2000);
+  }
+
+  getRandomStatus(): ServerStatus {
+    const statuses: ServerStatus[] = ['offline', 'unknown', 'online'];
+    const randomIndex = Math.floor(Math.random() * statuses.length);
+    return statuses[randomIndex];
+  }
 }
---
+++ my-angular-course-18/lifecycle-deep-dive-start/src/app/lifecycle/lifecycle.component.ts
@@ -0,0 +1,73 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  standalone: true,
+  imports: [],
+  templateUrl: './lifecycle.component.html',
+  styleUrl: './lifecycle.component.css',
+})
+export class LifecycleComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() text?: string;
+
+  constructor() {
+    console.log('CONSTRUCTOR');
+    console.log('CONSTRUCTOR text:', this.text); // undefined
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log('ngOnInit text:', this.text);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit'); // Content - wszystko co znajduje w <ng-content>
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit'); // View - wszystko co znajduje sie w html (też content)
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+}
