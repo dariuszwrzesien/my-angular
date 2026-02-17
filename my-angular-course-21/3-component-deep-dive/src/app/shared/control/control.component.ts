db656723f624cf5f4db2ca99508e2cfaddf5199e
Dariusz Wrzesien <dwrzesien@ccaeducate.me>
2026-02-17 11:07:16 +0100

    feat: refactor dashboard item and control components for improved structure and styling

--- my-angular-course-21/3-component-deep-dive/src/app/shared/control/control.component.ts
+++ my-angular-course-21/3-component-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,13 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Host,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,7 +16,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, //disable view encapsulation to allow CSS styles to be applied globally
+  // host: {
+  //   class: 'control',
+  // },
 })
 export class ControlComponent {
+  @HostBinding('class') className = 'control'; //binduje klasę CSS 'control' do elementu hosta, co pozwala na stosowanie stylów CSS do tego elementu
+  @HostListener('click') onClickHost() {
+    //nasłuchiwanie zdarzenia click na elemencie hosta i wywołanie metody onClick
+    this.onClick();
+  }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Control clicked');
+    console.log(this.el.nativeElement); //dostęp do elementu DOM, na którym nasłuchujemy zdarzenia click, i wyświetlenie go w konsoli
+  }
 }
